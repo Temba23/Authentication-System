d7a753f75a016be643b7ca842a34a130fcfecde7
Temba23 <tembasherpa80@gmail.com>
2025-09-01 20:33:35 +0545

    Build error : requirements and pip error

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ set -o errexit
 
 pip install --upgrade pip setuptools wheel
 
-pip install -r requirements.txt
+pip install --only-binary=:all: -r requirements.txt
+
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
