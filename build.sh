75e390530901edb5ca19781da2659fa61a198914
Temba23 <tembasherpa80@gmail.com>
2025-09-01 19:57:27 +0545

    asgi and wsgi settings file changes, build and render documents installation

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
+# Exit on error
+set -o errexit  
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Run migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
